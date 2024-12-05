349f9b7a5326e96ee0e1ecf8483915112c098891
John <John@DESKTOP-OPFHU6D>
2024-12-04 22:09:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wishcart',
+  webDir: 'dist'
+};
+
+export default config;
